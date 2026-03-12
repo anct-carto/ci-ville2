a3e19767dfbb333aa631421d3bbb837a340ae4df
lunapelchat <luna-marie.pelchat@anct.gouv.fr>
2026-03-12 13:54:12 +0100

    scr deploy

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,12 @@
 const webpack = require('webpack')
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    // publicPath: process.env.NODE_ENV === 'production'
-    //   ? '/' + 'vie-associative' + '/'
-    //   : '/'
-    publicPath:'',
-    configureWebpack: {
-        plugins: [
-            // ...
-            new webpack.DefinePlugin({
-              'process.env.NODE_ENV': JSON.stringify('production')
-            }),
-            // new BundleAnalyzerPlugin()
-        ],
-    }
+  publicPath: '/ci-ville2/',
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      })
+    ]
+  }
 }
